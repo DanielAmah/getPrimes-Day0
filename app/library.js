47bf998b30066d40ce66409473addb1603d82593
DanielAmah <amahdanieljack@gmail.com>
2017-05-08 20:38:25 -0700

    commit all files to master branch

---
+++ app/library.js
@@ -0,0 +1,45 @@
+'use strict'
+
+ const getPrimes = (num)=>{
+ 
+	 let checkPrime=(num) =>{
+		 
+		for(let i = 2; i<=num; i++){
+			if(num%i===0 && num !==i)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	 }
+	 let primeArray = [];
+	 
+	 if (num < 0) 
+	 {
+		return "negative integers can not be prime";
+	 }
+	 else if (num === 1) 
+	 {
+		return '1 is not a prime number';  
+	 }
+	 else if (num === 2) 
+	 {
+		return [2];
+	 }
+	  
+	 else 
+	 {
+	  
+		 for(let i = 2; i<=num; i++)
+		 {
+			if(checkPrime(i)===true)
+			{
+				primeArray.push(i);
+			}		
+		 }
+		return primeArray;
+	  }
+ }
+ 
+module.exports = getPrimes;
